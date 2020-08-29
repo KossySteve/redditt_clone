56eea1d59d2dbf1ba9d78dda6722f95f4139544b
mimi peshy <mimipeshy@gmail.com>
2020-08-29 16:28:57 +0300

    fixed invalid credentials like nil in post and comment'

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :title, presence: true, uniqueness: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { minimum: 4, maximum: 100 }
+  validates :body, presence: true,length: { minimum: 4, maximum: 300 }
 end
